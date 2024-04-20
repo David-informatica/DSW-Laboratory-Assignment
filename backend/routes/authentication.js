8133af2d3ee5aa521ceaa7ebe9bb0608d62438d1
jesuusgpmp14 <146329767+jesuusgpmp14@users.noreply.github.com>
2024-04-20 10:39:39 +0200

    Update authentication.js

--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -2,36 +2,44 @@ import express from 'express'
 const router = express.Router()
 import UserSchema from '../models/User.js'
 
-//LOoin user
+// Login user
 router.post('/login', async (req, res) => {
-  const { email, password } = UserSchema(req.body);
+  const { email, password } = req.body; // Asegúrate de extraer directamente desde req.body
 
   try {
-    // Busca el usuario en la base de datos por nombre de usuario y password
-    const usuario = await UserSchema.findOne({ email, password });
+    // Busca el usuario en la base de datos por email y password
+    const user = await UserSchema.findOne({ email, password });
 
-    // Si el usuario no existe o la password es incorrecta, devuelve un error
-    if (!usuario) {
+    // Si el usuario no existe o la contraseña es incorrecta, devuelve un error
+    if (!user) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    // Si el usuario existe y la password es correcta, devuelve un mensaje de éxito
-    res.status(200).json({ mensaje: 'Inicio de sesión exitoso', usuario });
+    // Si el usuario existe y la contraseña es correcta, devuelve los datos del usuario
+    // Asegúrate de que la información sensible no sea devuelta
+    const userData = {
+      name: user.name, // Asumiendo que tienes un campo 'name' en tu modelo
+      email: user.email // y un campo 'email'
+    };
 
+    res.status(200).json({ message: 'Login successful', user: userData });
   } catch (error) {
     // Manejo de errores
     console.error(error);
-    res.status(500).json({ error: 'Error del servidor' });
+    res.status(500).json({ error: 'Server error' });
   }
 });
 
-//Register User
-router.post('/register', (req, res) => {
-  const user = UserSchema(req.body);
-  user
-    .save()
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }));
-})
+// Register User
+router.post('/register', async (req, res) => {
+  const newUser = new UserSchema(req.body);
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json({ message: 'User registered', user: { name: savedUser.name, email: savedUser.email }});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error registering user' });
+  }
+});
 
 export default router;
